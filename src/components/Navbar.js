4008640f8c22a4b0984c06ba275d6bd5c2324278
david bassey <davidosky1@gmail.com>
2021-07-03 19:50:21 +0100

    Add navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+const Navbar = () => (
+  <header className="header__wrapper">
+    <Link to="/">PetFinder</Link>
+  </header>
+);
+export default Navbar;
