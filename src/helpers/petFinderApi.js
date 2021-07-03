1e63b2857f7c49018786c1c52169ef5ce4798002
david bassey <davidosky1@gmail.com>
2021-07-03 19:56:57 +0100

    Add petFinderApi.js file

---
+++ src/helpers/petFinderApi.js
@@ -0,0 +1,8 @@
+import { Client } from '@petfinder/petfinder-js';
+
+const client = new Client({
+  apiKey: 'beSzIv6cUFbXg90ZkcZfofapP6di6svCmw3iusKJdNMOIaXW2a',
+  secret: 'p1XW3TqKCjyuOMQtMbbDSBxbOY2poOtn57IOtsL6',
+});
+
+export default client;
