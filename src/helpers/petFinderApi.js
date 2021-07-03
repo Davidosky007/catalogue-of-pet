ebae32467338382ebfa7baa0597b519c28123010
david bassey <davidosky1@gmail.com>
2021-07-03 20:06:50 +0100

    Create store

--- src/helpers/petFinderApi.js
+++ src/helpers/petFinderApi.js
@@ -1,8 +1,8 @@
 import { Client } from '@petfinder/petfinder-js';
 
 const client = new Client({
-  apiKey: 'beSzIv6cUFbXg90ZkcZfofapP6di6svCmw3iusKJdNMOIaXW2a',
-  secret: 'p1XW3TqKCjyuOMQtMbbDSBxbOY2poOtn57IOtsL6',
+  apiKey: 'Fdf8bq5IxkbNvy33sZyJHGgPqvQi1IbLJmfYRWXyGqg8brdlRj',
+  secret: 'IwaMWvroxBatU6O02gWZ2EQx38sIvVPkSeTrZgn4',
 });
 
 export default client;
