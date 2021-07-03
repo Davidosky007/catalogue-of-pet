6d02813ec6541204289dc74e20477655ff07a5f7
david bassey <davidosky1@gmail.com>
2021-07-03 19:44:32 +0100

    Add action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,39 @@ export const ADD_PETS = 'ADD_PETS';
 export const FILTER_PETS = 'FILTER_PETS';
 export const FILTER_SIZE = 'FILTER_SIZE';
 export const FILTER_GENDER = 'FILTER_GENDER';
+
+
+export const addPets = pets => ({
+  type: ADD_PETS,
+  payload: {
+    pets,
+  },
+});
+
+
+export const fetchPets = (animals) => {
+  return (dispatch) => {
+    return dispatch(addPets(animals || []));
+  };
+};
+
+export const filterPets = breed => ({
+  type: FILTER_PETS,
+  payload: {
+    breed,
+  },
+});
+
+export const filterSize = size => ({
+  type: FILTER_SIZE,
+  payload: {
+    size,
+  },
+});
+
+export const filterGender = gender => ({
+  type: FILTER_GENDER,
+  payload: {
+    gender,
+  },
+});
