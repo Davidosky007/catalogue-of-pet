c2a344eefbb086c7e31989873fe11498604ada5f
david bassey <davidosky1@gmail.com>
2021-07-03 19:43:23 +0100

    Add action types

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export const ADD_PETS = 'ADD_PETS';
+export const FILTER_PETS = 'FILTER_PETS';
+export const FILTER_SIZE = 'FILTER_SIZE';
+export const FILTER_GENDER = 'FILTER_GENDER';
--- src/containers/SearchPetContainer.js
+++ src/containers/SearchPetContainer.js
@@ -12,5 +12,10 @@ const mapStateToProps = (state) => ({
 });
 
 const mapStateToDispatch = (dispatch) => ({
-  
-})
+  fetchPets: (animals) => dispatch(fetchPets(animals)),
+  filterPets: (breed) => dispatch(filterPets(breed)),
+  filterSize: (size) => dispatch(filterSize(size)),
+  filterGender: (gender) => dispatch(filterGender(gender)),
+});
+
+export default connect(mapStateToProps, mapStateToDispatch)(SearchPets);
